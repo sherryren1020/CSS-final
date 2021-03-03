b417ae30d38d4b54810ced281bfce81d19d2da92
Shuwei Ren <w0444586@campus.nscc.ca>
2021-03-02 23:47:53 -0400

    init commit

---
+++ p3/script.js
@@ -0,0 +1,63 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader')
+
+// show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// hide loading
+function complete() {
+    if (!loader.hidden) {
+        quoteContainer.hidden = false;
+        loader.hidden = true;
+
+    }
+}
+// get quote from API
+async function getQuote() {
+    loading()
+    const proxyUrl = 'http://cors-anywhere.herokuapp.com/'
+    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
+    try {
+        const response = await fetch(proxyUrl + apiUrl);
+        const data = await response.json();
+        //IF Author is blank, Add 'Unknown'
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown';
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote')
+        }
+        // authorText.innerText = data.quoteAuthor;
+        quoteText.innerText = data.quoteText;
+        complete();
+        // stop loader and show quote.
+
+    } catch (error) {
+        getQuote()
+    }
+}
+
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerText;
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} -
+    ${author}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// EVENT LISTENERS
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+// on load
+getQuote();
